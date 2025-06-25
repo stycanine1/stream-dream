d86d0c675d1c2d3ae03b2bfc44208d144c500003
Grey <samxerz1@gmail.com>
2025-06-26 06:46:02 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '76324910ca15f543396c779ace8cb604';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
